287f06d7d90730a8b7e3db5ae4b26b3ae8b3b572
daw1programacion <infosmr2@gmail.com>
2019-09-12 13:00:17 +0200

    12 septiembre - 13

---
+++ CRUDMVC/src/modelo/Conexion.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author usuario
+ */
+public class Conexion {
+    private final String base = "tienda";
+    private final String user = "root";
+    private final String password = "admin";
+    private final String url ="jdbc:mysql://localhost:3306/" + base;
+    private final Conexion con = null;
+    
+}
